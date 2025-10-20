28260fcf8a48aef715b77a7e7083ada040e73aad
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-10-20 18:19:54 +0200

    Added basic VR scene in component

---
+++ src/components/VRScene.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-scene>
+    <a-box position="0 1 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+    <a-sky color="#000"></a-sky>
+    <a-entity position="0 1.6 0">
+      <a-camera wasd-controls look-controls></a-camera>
+    </a-entity>
+  </a-scene>
+</template>
